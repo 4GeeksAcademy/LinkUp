a791f7328d2467f1ac016a584e4efc1dfb73b879
Domingo Ramírez Morales <128168900+Rzmsdo@users.noreply.github.com>
2025-03-03 15:20:11 +0000

    Mejora en Frontend

--- src/front/js/pages/signUp.js
+++ src/front/js/pages/signUp.js
@@ -91,6 +91,7 @@ export const SignUp = () => {
                             </div>
                         </div>
                         <div className="p-0 col-md-8 col-lg-9 col-xl-10 mb-3 mx-auto">
+                        <p className="" id="Help">¿Tienes una cuenta? Haz login <a className="text-warning" href="/login">aqui</a></p>
                             <button type="submit" className="acceso col-5 ms-0">Registrarse</button>
                             
                         </div>

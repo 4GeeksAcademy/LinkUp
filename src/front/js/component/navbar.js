b72de3d4876deae2c50479a19dc3cab57cab2935
nachodev <159072179+nachodev30@users.noreply.github.com>
2025-03-05 19:14:23 +0000

    navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,13 +3,27 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				
-				<div className="ml-auto">
-					
-				</div>
-			</div>
-		</nav>
+		<nav className="navbar navbar-expand-lg navbar-light bg-light px-4">
+      <div className="d-flex align-items-center">
+        <img
+          src="/path-to-logo.png"
+          alt="Company Logo"
+          className="rounded-circle"
+          width="40"
+          height="40"
+        />
+        <span className="ms-2 fw-bold">LinkUp</span>
+      </div>
+      <div className="ms-auto d-flex align-items-center">
+        <span className="me-2">Hi, Usuario</span>
+        <img
+          src="/path-to-user.png"
+          alt="User Avatar"
+          className="rounded-circle"
+          width="40"
+          height="40"
+        />
+      </div>
+    </nav>
 	);
 };

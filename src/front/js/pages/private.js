236cfba12a8dba5317b1bbac74a282a85c5da9e3
Domingo Ramírez Morales <128168900+Rzmsdo@users.noreply.github.com>
2025-03-18 09:37:12 +0000

    ac

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -312,29 +312,7 @@ export const Private = () => {
                     </div>
                 </div>
             </div>
-            {/* <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                
-            </button> */}
-
-            {/* <!-- Modal Unirme a grupo--> */
-            /* // <div className="modal fade" id="staticBackdrop2" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-2" aria-labelledby="staticBackdropLabel2" aria-hidden="true">
-            //     <div className="modal-dialog">
-            //         <div className="modal-content">
-            //             <div className="modal-header">
-            //                 <h1 class="modal-title fs-5" id="staticBackdropLabel2">Unirse a un grupo</h1>
-            //                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            //             </div>
-            //             <div className="modal-body">
-            //                 ...
-            //             </div>
-            //             <div className="modal-footer">
-            //                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancela</button>
-            //                 <button type="button" className="btn btn-primary">Unirme al grupo</button>
-            //             </div>
-            //         </div>
-            //     </div>
-            // </div> */}
+         
 
 
 
